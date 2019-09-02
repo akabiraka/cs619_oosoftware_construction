8ef2f3970b613dc9ad3a41d87b59c68f531acea3
akabir <akabiraneeq@gmail.com>
2019-09-02 16:40:22 -0400

    javadoc added in months function in Loan class, removed the edge-conditions as preconditions in the method contracts

--- Assignment1/src/Loan.java
+++ Assignment1/src/Loan.java
@@ -1,25 +1,27 @@
-
+/**
+ * Assignment 1 
+ * @author akabir
+ * @version 1.0
+ */
 public class Loan {
+	
+	/**
+	 * Calculate the number of months to pay off the principal.
+	 * @param principal The loan amount taken. Requires positive integer value.
+	 * @param annualInterestRate The annual interest rate in decimal point. Requires positive decimal value.
+	 * @param monthlyPayment The monthly payment amount. Requires positive integer value. 
+	 * It should be sufficiently large to drive the principal to zero.
+	 * @return The number of months to pay off the principal loan amount. 
+	 */
 	public static int months(int principal, double annualInterestRate, int monthlyPayment) {
-
-		double monthlyInterestRate = (annualInterestRate / 100) / 12;
+		double monthlyInterestRate = annualInterestRate / 12;
 		int months = 0;
-
-		if (principal < 0 || annualInterestRate < 0 || monthlyInterestRate < 0)
-			System.out.println("Illigal argument.");
-		else {
-			double oldPrincipal = (double) principal;
-			double newPrincipal = oldPrincipal * (1 + monthlyInterestRate) - monthlyPayment;
-			if (newPrincipal > oldPrincipal) {
-				System.out.println("Payment is not sufficiently large to drive the principal to zero.");
-			} else {
-				newPrincipal = 0.0;
-				while (oldPrincipal > 0) {
-					newPrincipal = oldPrincipal * (1 + monthlyInterestRate) - monthlyPayment;
-					oldPrincipal = newPrincipal;
-					months++;
-				}
-			}
+		double oldPrincipal = (double) principal;
+		double newPrincipal = 0.0;
+		while (oldPrincipal > 0) {
+			newPrincipal = oldPrincipal * (1 + monthlyInterestRate) - monthlyPayment;
+			oldPrincipal = newPrincipal;
+			months++;
 		}
 		return months;
 	}
--- Assignment1/src/Main.java
+++ Assignment1/src/Main.java
@@ -3,10 +3,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(Loan.months(100000, 8, 1000));
-		System.out.println(Loan.months(100000, 8, 1100));
-		System.out.println(Loan.months(100000,  8, 100));
-		System.out.println(Loan.months(-10000, 8, 1000));
+		System.out.println(Loan.months(100000, .08, 1000));
+		System.out.println(Loan.months(100000, .08, 1100));
+		//System.out.println(Loan.months(100000, 8, 100));
+		//System.out.println(Loan.months(-10000, 8, 1000));
 	}
 
 }
