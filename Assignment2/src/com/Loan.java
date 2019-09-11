cc842c8ae176d57227ebe21853895eaae2896ba7
akabir <akabiraneeq@gmail.com>
2019-09-10 22:32:51 -0400

    assignment 2 updated

--- Assignment2/src/com/Loan.java
+++ Assignment2/src/com/Loan.java
@@ -1,34 +1,43 @@
 package com;
 
 public class Loan {
-	
+
 	/**
-	 * This method calculates the number of months needed to pay off the principal amount.
-	 * @param principal The principal amount.
+	 * This method calculates the number of months needed to pay off the principal
+	 * amount.
+	 * 
+	 * @param principal          The principal amount.
 	 * @param annualInterestRate The annual interest rate.
-	 * @param monthlyPayment The monthly payment amount.
+	 * @param monthlyPayment     The monthly payment amount.
 	 * @return the number of months required to pay off the principal.
-	 * @throws IllegalArgumentException if any of the arguments is not positive.
-	 * @throws IllegalArgumentException if monthlyPayment amount is not sufficiently
-	 * large to drive the principal to zero.
+	 * @throws IllegalArgumentException if any of principal, annualInterestRate or
+	 *                                  monthlyPayment is not positive. Or
+	 *                                  monthlyPayment amount is not sufficiently
+	 *                                  large to drive the principal to zero.
 	 */
 	public static int months(int principal, double annualInterestRate, int monthlyPayment) {
-		
-		if(principal <0 || annualInterestRate < 0 || monthlyPayment <0)
-			throw new IllegalArgumentException("All arguments should be positive.");
-		
+
+		if (principal < 0)
+			throw new IllegalArgumentException("Principal" + principal + " can not be negative.");
+
+		if (annualInterestRate < 0)
+			throw new IllegalArgumentException("Annual interest rate " + annualInterestRate + " can not be negative.");
+
+		if (monthlyPayment < 0)
+			throw new IllegalArgumentException("Monthly payment amount " + monthlyPayment + " can not be negative.");
+
 		double monthlyInterestRate = annualInterestRate / 12;
+		if (monthlyPayment <= principal * monthlyInterestRate)
+			throw new IllegalArgumentException("Payment " + monthlyPayment + " should be more than "
+					+ principal * monthlyInterestRate + " to drive principal to zero.");
+
 		int months = 0;
 		double currentPrincipal = (double) principal;
-		
-		if (currentPrincipal < currentPrincipal * (1+monthlyInterestRate) - monthlyPayment)
-			throw new IllegalArgumentException("Monthly payment should be sufficiently large.");
-		
+
 		while (currentPrincipal > 0) {
 			currentPrincipal = currentPrincipal * (1 + monthlyInterestRate) - monthlyPayment;
 			months++;
 		}
 		return months;
 	}
-
 }
