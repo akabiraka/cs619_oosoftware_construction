d306cb804c34cb1cab2cb1be44d5c30285078cc4
akabiraka <akabir0101@gmail.com>
2019-10-20 21:45:29 -0400

    assignment 8 added

---
+++ Assignment8/src/Generic/BoundedQueue.java
@@ -0,0 +1,175 @@
+package Generic;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Bounded queue representation of type E of given size. If two items a and b is
+ * inserted into this such that a comes before b, while serving a must be served
+ * before b.
+ * 
+ * AF(c) = c[0], c[1], ... ... c[n] where n=size-1. rep-inv: 1. c!=null 2.
+ * c[i]!=null for 0 <= i < size-1 3. size >= 0 4. c.size <= size
+ *
+ * @param <E> The Type of the elements in this.
+ */
+public class BoundedQueue<E> {
+	private List<E> rep;
+	private int size = 0;
+
+	/**
+	 * Create an empty bounded queue of given size.
+	 * 
+	 * @param size The given size of this.
+	 * @throws IllegalArgumentException if size is negative.
+	 */
+	public BoundedQueue(int size) {
+		if (size < 0)
+			throw new IllegalArgumentException("BoundedQueue: queue size can not be negative");
+		this.size = size;
+		this.rep = new ArrayList<E>();
+	}
+
+	/**
+	 * Checks whether this is empty or not.
+	 * 
+	 * @return true if this is empty, else false.
+	 */
+	public boolean isEmpty() {
+		return this.rep.isEmpty();
+	}
+
+	/**
+	 * Checks whether this is full or not.
+	 * 
+	 * @return true if this is full, else false.
+	 */
+	public boolean isFull() {
+		return this.rep.size() == this.size;
+	}
+
+	/**
+	 * Calculate the number of items in this.
+	 * 
+	 * @return the number of items in this.
+	 */
+	public int getCount() {
+		return this.rep.size();
+	}
+
+	/**
+	 * Calculate the size of this.
+	 * 
+	 * @return the size of this.
+	 */
+	public int getSize() {
+		return this.size;
+	}
+
+	/**
+	 * Put item e into this.
+	 * 
+	 * @param e the item to put into this.
+	 * @throws NullPointerException  if e is null.
+	 * @throws IllegalStateException if this is full while putting e into this.
+	 */
+	public void put(E e) {
+		if (e == null)
+			throw new NullPointerException("BoundedQueue.put: Can not put null into queue.");
+		if (isFull())
+			throw new IllegalStateException("BoundedQueue.put: The queue is full.");
+
+		this.rep.add(e);
+	}
+
+	/**
+	 * Return the first item of this.
+	 * 
+	 * @return the first item of this.
+	 * @throws IllegalStateException if this is empty while getting the first item
+	 *                               of this.
+	 */
+	public E get() {
+		if (isEmpty())
+			throw new IllegalStateException("BoundedQueue.get: The queue is empty.");
+		E e = this.rep.get(0);
+		this.rep.remove(0);
+		return e;
+	}
+
+	/**
+	 * @param elements a collection of items to put into this.
+	 * @throws NullPointerException     if null elements is passed to put into this.
+	 * @throws NullPointerException     if any item of elements is null.
+	 * @throws IllegalArgumentException if the collection size of items exceeds this
+	 *                                  size.
+	 * @throws IllegalArgumentException if the collection passed is empty.
+	 */
+	public void putAll(Iterable<? extends E> elements) {
+		if (elements == null)
+			throw new NullPointerException("BoundedQueue.putAll: Can not put null into queue.");
+		List<E> list = new ArrayList<E>();
+		for (E e : elements) {
+			if (e == null)
+				throw new NullPointerException("BoundedQueue.putAll: Can not put null into queue.");
+			list.add(e);
+		}
+
+		if (list.isEmpty()) {
+			throw new IllegalArgumentException("BoundedQueue.putAll: Nothing to put into queue.");
+		}
+
+		if (list.size() + this.rep.size() > this.size) {
+			throw new IllegalArgumentException("BoundedQueue.putAll: Not have enough space in this.");
+		}
+		this.rep.addAll(list);
+	}
+
+	/**
+	 * Put all items into destination collection from this.
+	 * 
+	 * @param collection The output collection where all elements of this will be
+	 *                   put.
+	 * @throws NullPointerException if the destination collection is null.
+	 */
+	public void getAll(Collection<? super E> collection) {
+		if (collection == null)
+			throw new NullPointerException("BoundedQueue:getAll: destination collection can not be null");
+		if (isEmpty())
+			throw new IllegalStateException("BoundedQueue.getAll: The queue is empty.");
+
+		collection.addAll(this.rep);
+		this.rep.clear();
+	}
+
+	/**
+	 * Checks whether this is ok or not.
+	 * 
+	 * @return true if this is ok, else false.
+	 */
+	public boolean repOk() {
+		return this.rep != null && !this.rep.contains(null) && this.size >= 0 && this.rep.size() <= this.size;
+	}
+
+	/**
+	 * Produce the string representation of this.
+	 * 
+	 * @return the string representation of this.
+	 */
+	@Override
+	public String toString() {
+		return this.rep.toString();
+	}
+
+	public static void main(String args[]) {
+		BoundedQueue<Number> queue = new BoundedQueue<Number>(10);
+		for (int i = 0; !queue.isFull(); i++) {
+			queue.put(Integer.valueOf(i));
+			System.out.println("put: " + i);
+		}
+		while (!queue.isEmpty()) {
+			System.out.println("get: " + queue.get());
+		}
+	}
+}
---
+++ Assignment8/src/Generic/UnitTests.java
@@ -0,0 +1,102 @@
+package Generic;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class UnitTests {
+
+	@Test
+	void test_queueCreation() {
+		// negative-sized buffer creation not allowed
+		assertThrows(IllegalArgumentException.class, () -> {
+			BoundedQueue<String> q = new BoundedQueue<String>(-5);
+		});
+
+		// zero-sized buffer-creation allowed
+		BoundedQueue<String> q = new BoundedQueue<String>(0);
+		assertEquals(0, q.getSize());
+
+		// happy queue
+		BoundedQueue<Number> queue = new BoundedQueue<Number>(10);
+		for (int i = 0; !queue.isFull(); i++) {
+			queue.put(Integer.valueOf(i));
+			System.out.println("put: " + i);
+		}
+		while (!queue.isEmpty()) {
+			System.out.println("get: " + queue.get());
+		}
+
+		assertTrue(queue.repOk());
+	}
+
+	@Test
+	void test_typeParamE() {
+		// we can put subtype of type parameter E in queue.
+		BoundedQueue<Number> q = new BoundedQueue<Number>(10);
+		Integer integer = Integer.valueOf(1);
+		Double double1 = Double.valueOf(1.111);
+		q.put(integer);
+		q.put(double1);
+		Number aNumber = q.get();
+		Number bNumber = q.get();
+		assertEquals(0, q.getCount());
+		assertTrue(q.repOk());
+	}
+
+	@Test
+	void test_boundedWildcard() {
+		BoundedQueue<Number> q = new BoundedQueue<Number>(10);
+		List<Integer> integers = Arrays.asList(1, 2, 3, 4, 5);
+		q.putAll(integers);
+		List<Double> doubles = Arrays.asList(1.1, 2.2, 3.3);
+		q.putAll(doubles);
+		List<Number> numbers = new ArrayList<Number>();
+		q.getAll(numbers);
+		System.out.println(numbers);
+		assertEquals(integers.size() + doubles.size(), numbers.size());
+		assertTrue(q.repOk());
+	}
+
+	@Test
+	void test_emptyQ() {
+		BoundedQueue<Number> q = new BoundedQueue<Number>(0);
+		assertThrows(IllegalStateException.class, () -> q.put(5)); // putting an item into empty queue
+		assertThrows(IllegalStateException.class, () -> q.get()); // getting an item from empty queue
+		List<Number> numbers = new ArrayList<Number>();
+		assertThrows(IllegalStateException.class, () -> q.getAll(numbers)); // getting all items from empty queue
+		numbers.add(5.5);
+		numbers.add(0);
+		assertThrows(IllegalArgumentException.class, () -> q.putAll(numbers)); // putting a collection into empty queue
+		assertTrue(q.repOk());
+	}
+
+	@Test
+	void test_nullAndEmptyOperation() {
+		BoundedQueue<String> q = new BoundedQueue<String>(10);
+		assertThrows(NullPointerException.class, () -> q.put(null)); // putting null value into queue
+		List<String> stringList = Arrays.asList(null, null, null);
+		assertThrows(NullPointerException.class, () -> q.putAll(stringList)); // putting a collection that contains null value into queue
+		List<String> stringList2 = new ArrayList<String>();
+		assertThrows(IllegalArgumentException.class, () -> q.putAll(stringList2)); // putting an empty collection into queue
+		List<String> stringList3 = null;
+		assertThrows(NullPointerException.class, () -> q.getAll(stringList3)); // passing null destination to receive all items from queue 
+		assertTrue(q.repOk());
+	}
+	
+	@Test
+	void test_fullQ() {
+		BoundedQueue<String> q = new BoundedQueue<String>(2);
+		q.put("cat");
+		q.put("dog");
+		assertThrows(IllegalStateException.class, () -> q.put("elephant")); // putting an item into full queue
+		List<String> stringList = Arrays.asList("mouse", "bird");
+		assertThrows(IllegalArgumentException.class, () -> q.putAll(stringList)); // putting a collection into full queue 
+		assertTrue(q.repOk());
+	}
+
+}
