cfe1e26d3340189c1dfe3318634cc1df8e6958a4
akabiraka <akabir0101@gmail.com>
2019-11-16 00:48:37 -0500

    bloch ch4 practice examples added

---
+++ FunBloch/src/com/ch4/UseOneClass.java
@@ -0,0 +1,15 @@
+package com.ch4;
+
+
+public class UseOneClass {
+//	private XPoint point;
+//	
+//	private static class XPoint() {
+//		private int x;
+//		private int y;
+//		public XPoint(int x, int y){
+//			this.x= x;
+//			this.y = y;
+//		}
+//	}
+}
